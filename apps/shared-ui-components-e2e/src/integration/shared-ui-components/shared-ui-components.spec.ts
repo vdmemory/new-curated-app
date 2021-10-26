448e43fb442dff7cc854055998801deb720eb36a
vdmemory <vdmemory2@gmail.com>
2021-10-26 12:38:51 +0300

    refactor

--- apps/shared-ui-components-e2e/src/integration/shared-ui-components/shared-ui-components.spec.ts
+++ apps/shared-ui-components-e2e/src/integration/shared-ui-components/shared-ui-components.spec.ts
@@ -1,7 +1,7 @@
 describe('shared-ui-components: SharedUiComponents component', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=shareduicomponents--primary'));
-    
+    beforeEach(() => cy.visit('/iframe.html?id=shareduicomponents--primary'));
+
     it('should render the component', () => {
-      cy.get('h1').should('contain', 'Welcome to SharedUiComponents!');
+        cy.get('h1').should('contain', 'Welcome to SharedUiComponents!');
     });
 });
