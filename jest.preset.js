cd6567a78eca56ba30f2ba8a41fa56fee54fa8c1
vdmemory <vdmemory2@gmail.com>
2021-10-26 15:02:29 +0300

    refactoring jest config

--- jest.preset.js
+++ jest.preset.js
@@ -2,16 +2,6 @@ const nxPreset = require('@nrwl/jest/preset');
 
 module.exports = {
     ...nxPreset,
-    // testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
-    // transform: {
-    //     '^.+\\.(ts|js|html)$': 'ts-jest',
-    // },
-    // resolver: '@nrwl/jest/plugins/resolver',
-    // moduleFileExtensions: ['ts', 'js', 'html'],
-    // moduleNameMapper: {
-    //     '^.+\\.svg$': `${jestHelpersDir}/svgrMock.js`,
-    //     '^.+\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `${jestHelpersDir}/fileMock.js`,
-    // },
     coverageReporters: ['text'],
     coverageThreshold: {
         global: {
