059725961e29f1eabe666f988154a6650baca3bd
vdmemory <vdmemory2@gmail.com>
2021-10-26 12:41:33 +0300

    update jest.preset

--- jest.preset.js
+++ jest.preset.js
@@ -2,12 +2,23 @@ const nxPreset = require('@nrwl/jest/preset');
 
 module.exports = {
     ...nxPreset,
+    // testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
+    // transform: {
+    //     '^.+\\.(ts|js|html)$': 'ts-jest',
+    // },
+    // resolver: '@nrwl/jest/plugins/resolver',
+    // moduleFileExtensions: ['ts', 'js', 'html'],
+    // moduleNameMapper: {
+    //     '^.+\\.svg$': `${jestHelpersDir}/svgrMock.js`,
+    //     '^.+\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `${jestHelpersDir}/fileMock.js`,
+    // },
+    coverageReporters: ['text'],
     coverageThreshold: {
         global: {
-            branches: 100,
-            functions: 100,
-            lines: 100,
-            statements: 100,
+            branches: 90,
+            functions: 90,
+            lines: 90,
+            statements: 90,
         },
     },
 };
