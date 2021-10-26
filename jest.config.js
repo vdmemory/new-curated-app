6b6898194056a8c6890dc3cfb1534ee0f449a148
vdmemory <vdmemory2@gmail.com>
2021-10-26 12:38:14 +0300

    added coveredge to jest.preset

--- jest.config.js
+++ jest.config.js
@@ -2,12 +2,4 @@ const { getJestProjects } = require('@nrwl/jest');
 
 module.exports = {
     projects: getJestProjects(),
-    coverageThreshold: {
-        global: {
-            branches: 75,
-            functions: 75,
-            lines: 75,
-            statements: 75,
-        },
-    },
 };
